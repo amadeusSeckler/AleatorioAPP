1211ed5c948029733a5d59340602432d54ad696c
Jonathan Calderon <jonathannahuelcalderon97@gmail.com>
2022-01-15 16:21:41 -0300

    Version estable get game

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000
 const filePath = 'ListaJuegos.txt'
 
 app.get('/', (req,res)=>{
-    res.json(manipulatorFile.getFileGames(filePath))
+    res.json({ games: manipulatorFile.getFileGames(filePath) })
 })
 
 app.listen(PORT, () => { console.log('App listening at port ' + PORT) })
