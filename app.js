268ddd14a20eabef69cafdb96a9cd7a016bd0579
Jonathan Calderon <jonathannahuelcalderon97@gmail.com>
2022-01-15 15:54:14 -0300

    initial commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const manipulatorFile = require('./functions')
 
-const port = 3000
+const port = process.env.port || 3000
 const filePath = 'ListaJuegos.txt'
 
 app.get('/', (req,res)=>{
